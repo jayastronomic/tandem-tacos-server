7c32ba1e2c2be285d853b69b7439878959e7a184
jayastronomic <61813086+jayastronomic@users.noreply.github.com>
2022-10-23 13:29:18 -0500

    Create Models: User, Recipe

---
+++ db/migrate/20221023093614_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name, null: false
+      t.string :description, null: false
+      t.references :user, null: false, type: :uuid
+      t.string :tags, array: true
+      t.string :ingredients, array: true
+      t.uuid :uuid, type: :uuid, default: -> { "gen_random_uuid()" }, null: false
+      t.timestamps
+    end
+    add_index :recipes, :name
+  end
+end
