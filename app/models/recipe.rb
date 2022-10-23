c672c52da7915330110de41d4041a6edc184e6e1
jayastronomic <61813086+jayastronomic@users.noreply.github.com>
2022-10-23 14:54:07 -0500

    Add Validations to models.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  validates :name, :description, presence: true
+  validates :name, length: { maximum: 200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :recipes
+
+  validates :name, :username, :email, presence: { strict: true }
+  validates :username, :email, uniqueness: { strict: true }
+  validates :username, format: { with: /\A[A-Za-z0-9_]+\z/ }
+  validates :name, length: { in: 1..100 }
+  validates :username, length: { in: 1..32 }
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
